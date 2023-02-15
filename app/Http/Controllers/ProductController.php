8aae47233fc528ac52523b788cd0491a9e3753ff
ductrong0411201 <ductronglu0411@gmail.com>
2023-02-15 10:33:10 +0700

    adsadsadsad

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Sanpham;
+use App\Models\Product;
 use Illuminate\Http\Request;
-use App\Models\Nhomsanpham;
+use App\Models\GroupProduct;
 use Illuminate\Support\Facades\File;
 
 class SanphamController extends Controller
@@ -17,10 +17,10 @@ class SanphamController extends Controller
     public function index()
     {
         if ($key=request()->key){
-            $data = Sanpham::where('ten', 'like', '%'.$key.'%')->orderby('uutien','DESC')->paginate(5);
+            $data = Product::where('name', 'like', '%'.$key.'%')->orderby('priority','DESC')->paginate(5);
         }
         else {
-            $data = Sanpham::orderby('uutien','DESC')->paginate(5);
+            $data = Product::orderby('priority','DESC')->paginate(5);
         }
 
         return view('admin.sanpham.index',['data'=>$data]);
@@ -33,7 +33,7 @@ class SanphamController extends Controller
      */
     public function create()
     {
-        $nhomsanphams=Nhomsanpham::orderby('ten')->where('trangthai',1)->select('id','ten')->get();
+        $nhomsanphams=GroupProduct::orderby('name')->where('status',1)->select('id','name')->get();
         return view('admin.sanpham.create', compact('nhomsanphams'));
     }
 
@@ -46,15 +46,15 @@ class SanphamController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'ten'=>'required|unique:sanpham',
-            'gia'=>'required',
-            'uutien'=>'required',
+            'name'=>'required|unique:sanpham',
+            'price'=>'required',
+            'priority'=>'required',
         ],
         [
-            'ten.required' => 'Cần nhập tên nhóm sản phẩm',
-            'ten.unique' => 'Tên nhóm sản phẩm cần duy nhất',
-            'gia.required' => 'Cần nhập giá sản phẩm',
-            'uutien.required' => "Cần nhập mức độ ưu tiên",
+            'name.required' => 'Cần nhập tên nhóm sản phẩm',
+            'name.unique' => 'Tên nhóm sản phẩm cần duy nhất',
+            'price.required' => 'Cần nhập giá sản phẩm',
+            'priority.required' => "Cần nhập mức độ ưu tiên",
 
         ]
         );
@@ -65,10 +65,10 @@ class SanphamController extends Controller
             $file_name=time().'-sp.'.$ext;
 
             $file->move(public_path('uploads'), $file_name);
-            $request->merge(['anh'=>$file_name]);
+            $request->merge(['image'=>$file_name]);
         }
 
-        if(Sanpham::create($request->all())){
+        if(Product::create($request->all())){
             return redirect()->route('admin.sanpham.index')->with('success', 'Thêm mới sản phẩm thành công.');
         }
     }
@@ -76,10 +76,10 @@ class SanphamController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Sanpham  $sanpham
+     * @param  \App\Models\Product  $sanpham
      * @return \Illuminate\Http\Response
      */
-    public function show(Sanpham $sanpham)
+    public function show(Product $sanpham)
     {
         //
     }
@@ -87,12 +87,12 @@ class SanphamController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Sanpham  $sanpham
+     * @param  \App\Models\Product  $sanpham
      * @return \Illuminate\Http\Response
      */
-    public function edit(Sanpham $sanpham)
+    public function edit(Product $sanpham)
     {
-        $nhomsanphams=Nhomsanpham::orderby('ten')->where('trangthai',1)->select('id','ten')->get();
+        $nhomsanphams=GroupProduct::orderby('name')->where('status',1)->select('id','name')->get();
 
         return view('admin.sanpham.edit', ['sanpham'=>$sanpham, 'nhomsanphams'=>$nhomsanphams]);
     }
@@ -101,27 +101,27 @@ class SanphamController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Sanpham  $sanpham
+     * @param  \App\Models\Product  $sanpham
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Sanpham $sanpham)
+    public function update(Request $request, Product $sanpham)
     {
         $request->validate([
-            'ten'=>'required|unique:nhomsanpham,ten,'.$sanpham->id,
-            'gia'=>'required',
-            'uutien'=>'required',
+            'name'=>'required|unique:nhomsanpham,ten,'.$sanpham->id,
+            'price'=>'required',
+            'priority'=>'required',
         ],
         [
-            'ten.required' => 'Cần nhập tên nhóm sản phẩm',
-            'ten.unique' => 'Tên nhóm sản phẩm cần duy nhất',
-            'gia.required' => 'Cần nhập giá sản phẩm',
-            'uutien.required' => "Cần nhập mức độ ưu tiên",
+            'name.required' => 'Cần nhập tên nhóm sản phẩm',
+            'name.unique' => 'Tên nhóm sản phẩm cần duy nhất',
+            'price.required' => 'Cần nhập giá sản phẩm',
+            'priority.required' => "Cần nhập mức độ ưu tiên",
 
         ]
         );
 
         $deleteimage=false;
-        $oldimage=public_path('uploads/'.$sanpham->anh);
+        $oldimage=public_path('uploads/'.$sanpham->image);
 
         if ($request->has('file_upload')){
             $file=$request->file_upload;
@@ -130,12 +130,12 @@ class SanphamController extends Controller
             $file_name=time().'-sp.'.$ext;
 
             $file->move(public_path('uploads'), $file_name);
-            $request->merge(['anh'=>$file_name]);
+            $request->merge(['image'=>$file_name]);
             $deleteimage=true;
         }else{
-            $request->merge(['anh'=>$sanpham->anh]);
+            $request->merge(['image'=>$sanpham->anh]);
         }
-        if($sanpham->update($request->only('ten','mota','danhsachanh','nhomsanphamid','gia','giaban','anh','trangthai','uutien'))){
+        if($sanpham->update($request->only('name','mota','danhsachanh','nhomsanphamid','price','priceban','image','status','priority'))){
             if ($deleteimage){
                 File::delete($oldimage);
             }
@@ -147,10 +147,10 @@ class SanphamController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Sanpham  $sanpham
+     * @param  \App\Models\Product  $sanpham
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Sanpham $sanpham)
+    public function destroy(Product $sanpham)
     {
         $sanpham->delete();
         return redirect()->route('admin.sanpham.index')->with('success','Xóa bản ghi thành công.');
